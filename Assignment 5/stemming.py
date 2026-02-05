8b6dd42a873ad2e5ddabee5ae6d5b793614cb661
Kaustub-Warme <kaustubhwarme@gmail.com>
2026-02-05 09:04:53 +0530

    assignment 5

--- Assignment 5/stemming.py
+++ Assignment 5/stemming.py
@@ -1,70 +1,39 @@
 import nltk
-nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
-from nltk.tokenize import (
-    WhitespaceTokenizer,
-    wordpunct_tokenize,
-    TreebankWordTokenizer,
-    TweetTokenizer,
-    MWETokenizer
-)
-from nltk.stem import PorterStemmer, SnowballStemmer, WordNetLemmatizer
 
-# Sample text
-text = "NLTK is great! I love Natural Language Processing :) #NLP @OpenAI"
-mwe_text = "I live in New York City and work on machine learning."
 
-print("Original Text:")
-print(text)
+from nltk.corpus import wordnet as wn
 
-# ---------------- TOKENIZATION ----------------
+# Input word
+word = "good"
 
-# 1. Whitespace Tokenization
-wt = WhitespaceTokenizer()
-print("\nWhitespace Tokenization:")
-print(wt.tokenize(text))
+print("Word:", word)
 
-# 2. Punctuation-based Tokenization
-print("\nPunctuation-based Tokenization:")
-print(wordpunct_tokenize(text))
+# ---------------- SYNONYMS ----------------
+synonyms = set()
+for syn in wn.synsets(word):
+    for lemma in syn.lemmas():
+        synonyms.add(lemma.name())
 
-# 3. Treebank Tokenization
-tbt = TreebankWordTokenizer()
-print("\nTreebank Tokenization:")
-print(tbt.tokenize(text))
+print("\nSynonyms:")
+print(synonyms)
 
-# 4. Tweet Tokenization
-tweet_tokenizer = TweetTokenizer()
-print("\nTweet Tokenization:")
-print(tweet_tokenizer.tokenize(text))
+# ---------------- ANTONYMS ----------------
+antonyms = set()
+for syn in wn.synsets(word):
+    for lemma in syn.lemmas():
+        if lemma.antonyms():
+            antonyms.add(lemma.antonyms()[0].name())
 
-# 5. MWE Tokenization
-mwe_tokenizer = MWETokenizer([('new', 'york', 'city'), ('machine', 'learning')], separator='_')
-print("\nMWE Tokenization:")
-print(mwe_tokenizer.tokenize(mwe_text.lower().split()))
+print("\nAntonyms:")
+print(antonyms)
 
-# ---------------- STEMMING ----------------
+# ---------------- HYPERNYMS ----------------
+hypernyms = set()
+for syn in wn.synsets(word):
+    for hyper in syn.hypernyms():
+        hypernyms.add(hyper.name())
 
-words = ["running", "runs", "runner", "easily", "fairly"]
-
-# Porter Stemmer
-porter = PorterStemmer()
-print("\nPorter Stemmer:")
-for word in words:
-    print(word, "->", porter.stem(word))
-
-# Snowball Stemmer
-snowball = SnowballStemmer("english")
-print("\nSnowball Stemmer:")
-for word in words:
-    print(word, "->", snowball.stem(word))
-
-# ---------------- LEMMATIZATION ----------------
-
-lemmatizer = WordNetLemmatizer()
-lemmatization_words = ["running", "better", "cars", "went"]
-
-print("\nLemmatization:")
-for word in lemmatization_words:
-    print(word, "->", lemmatizer.lemmatize(word))
+print("\nHypernyms:")
+print(hypernyms)
